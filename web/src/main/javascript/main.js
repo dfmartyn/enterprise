8df9e7ea38c86877da44a6a303f69429b9679235
Dmitriy Martynov <dfmartyn@gmail.com>
2017-08-20 23:03:04 +0300

    Add web-client

---
+++ web/src/main/javascript/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import * as $ from 'jquery';
+
+$(function () {
+    $('#mount-point').append('<p>Hello!</p>');
+});
---
+++ web/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/main/javascript/main.js",
+    output: {
+        path: __dirname + '/public/build/',
+        publicPath: "build/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: [/node/, /node_modules/, /public/]
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            }
+        ]
+    }
+};
